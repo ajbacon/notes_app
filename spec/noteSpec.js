322ca3021ba810f09a3486f754f12468f6bc72a7
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-06 16:10:07 +0000

    first test for note text

---
+++ spec/noteSpec.js
@@ -0,0 +1,15 @@
+// decribe ('Note', function () {
+//   var newNote = new Note ('this is a note')
+
+//   expect(newNote.showText()).toEq('this is a note')
+// })
+
+(function (exports) {
+  function noteHasTextWhenCreated() {
+    var newNote = new Note ('this is a note')
+    return assert.isTrue(newNote.showText() === 'this is a note')
+  }
+  exports.noteHasTextWhenCreated = noteHasTextWhenCreated
+})(this)
+
+console.log(noteHasTextWhenCreated())
