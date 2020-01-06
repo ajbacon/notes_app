0df9d39988aee083e78af663c715cfa654afc514
Hyan18 <Hyan18@github.com>
2020-01-06 17:05:16 +0000

    test and add noteListModel

---
+++ spec/noteListModelSpec.js
@@ -0,0 +1,11 @@
+(function (exports) {
+  function createNoteAddsNewNoteToList() {
+    var noteList = new NoteList ()
+    noteList.createNote('this is a note')
+
+    return assert.isTrue(noteList.showNotes()[0] instanceof Note)
+  }
+  exports.createNoteAddsNewNoteToList = createNoteAddsNewNoteToList
+})(this)
+
+console.log(createNoteAddsNewNoteToList())
