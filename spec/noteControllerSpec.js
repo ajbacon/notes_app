853e15463488bba5931b3ad6f4d6a47dce7952d3
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-07 17:25:31 +0000

    finish test for note controller

--- spec/noteControllerSpec.js
+++ spec/noteControllerSpec.js
@@ -2,9 +2,10 @@ describe('NoteController', () => {
   describe('.createNoteListView', () => {
     it('page content', () => {
       var noteController = new NoteController(new NoteList())
-      noteController.createNoteListView()
+      element = { innerHTML: 'something' }
+      noteController.createNoteListView(element)
       output = '<ul><li><div>hello my name is harry</div></li></ul>'
-      expect(document.getElementById.innerHTML).toEq(output)
+      expect(element.innerHTML).toEq(output)
     })
   })
 })
