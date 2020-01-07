df571ff93f7d4de88140b4113044215e0d102408
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-07 09:35:21 +0000

    refactored test code

--- spec/noteListViewSpec.js
+++ spec/noteListViewSpec.js
@@ -1,13 +1,27 @@
-(function (exports) {
-  function outputHTMLString () {
-    var noteList = new NoteList()
-    noteList.createNote('this is a note')
-    noteList.createNote('this is a second note')
-    var noteListView = new NoteListView(noteList)
+// (function (exports) {
+//   function outputHTMLString () {
+//     var noteList = new NoteList()
+//     noteList.createNote('this is a note')
+//     noteList.createNote('this is a second note')
+//     var noteListView = new NoteListView(noteList)
 
-    return assert.isTrue(noteListView.toHTML() === '<ul><li><div>this is a note</div></li><li><div>this is a second note</div></li></ul>')
-  }
-  exports.outputHTMLString = outputHTMLString
-})(this)
+//     return assert.isTrue(noteListView.toHTML() === '<ul><li><div>this is a note</div></li><li><div>this is a second note</div></li></ul>')
+//   }
+//   exports.outputHTMLString = outputHTMLString
+// })(this)
 
-console.log(outputHTMLString())
+// console.log(outputHTMLString())
+
+describe('NoteListView', () => {
+  describe('.toHTML', () => {
+    it('outputs Notes to html string', () => {
+      var noteList = new NoteList()
+      noteList.createNote('this is a note')
+      noteList.createNote('this is a second note')
+      var noteListView = new NoteListView(noteList)
+
+      var expectedOutput = '<ul><li><div>this is a note</div></li><li><div>this is a second note</div></li></ul>'
+      expect(noteListView.toHTML()).toEq(expectedOutput)
+    })
+  })
+})
--- spec/noteModelSpec.js
+++ spec/noteModelSpec.js
@@ -4,12 +4,25 @@
 //   expect(newNote.showText()).toEq('this is a note')
 // })
 
-(function (exports) {
-  function noteHasTextWhenCreated () {
-    var newNote = new Note('this is a note')
-    return assert.isTrue(newNote.showText() === 'this is a note')
-  }
-  exports.noteHasTextWhenCreated = noteHasTextWhenCreated
-})(this)
+// (function (exports) {
+//   function noteHasTextWhenCreated () {
+//     var newNote = new Note('this is a note')
+//     return assert.isTrue(newNote.showText() === 'this is a note')
+//   }
+//   exports.noteHasTextWhenCreated = noteHasTextWhenCreated
+// })(this)
 
-console.log(noteHasTextWhenCreated())
+// console.log(noteHasTextWhenCreated())
+
+describe('Note', () => {
+  it('has text upon creation', () => {
+    var newNote = new Note('this is a note')
+    expect(newNote.showText()).toEq('this is a note')
+  })
+})
+
+describe('test fail color', () => {
+  it('fails a test', () => {
+    expect(1).toEq(2)
+  })
+})
