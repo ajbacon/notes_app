e3cb8f07c6b3d62dca931291529cdfbb9f629768
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-07 11:31:16 +0000

    tidy up comments and obsolete code

--- spec/noteListModelSpec.js
+++ spec/noteListModelSpec.js
@@ -1,15 +1,3 @@
-// (function (exports) {
-//   function createNoteAddsNewNoteToList () {
-//     var noteList = new NoteList()
-//     noteList.createNote('this is a note')
-
-//     return assert.isTrue(noteList.showNotes()[0] instanceof Note)
-//   }
-//   exports.createNoteAddsNewNoteToList = createNoteAddsNewNoteToList
-// })(this)
-
-// console.log(createNoteAddsNewNoteToList())
-
 describe('NoteList', () => {
   describe('.createNote', () => {
     it('adds new note to a list', () => {
--- spec/noteListViewSpec.js
+++ spec/noteListViewSpec.js
@@ -1,17 +1,3 @@
-// (function (exports) {
-//   function outputHTMLString () {
-//     var noteList = new NoteList()
-//     noteList.createNote('this is a note')
-//     noteList.createNote('this is a second note')
-//     var noteListView = new NoteListView(noteList)
-
-//     return assert.isTrue(noteListView.toHTML() === '<ul><li><div>this is a note</div></li><li><div>this is a second note</div></li></ul>')
-//   }
-//   exports.outputHTMLString = outputHTMLString
-// })(this)
-
-// console.log(outputHTMLString())
-
 describe('NoteListView', () => {
   describe('.toHTML', () => {
     it('outputs Notes to html string', () => {
